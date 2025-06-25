304cec1d5bbe2000f9dc5b6c757848c4c4499bd1
omsaravanan1988 <91906734+omsaravanan1988@users.noreply.github.com>
2025-06-25 10:11:39 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,42 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-# Create a random name for the resource group using random_pet
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
-# Create a resource group using the generated random name
 resource "azurerm_resource_group" "example" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-##variables
-variable "resource_group_location" {
-  type        = string
-  default     = "eastus"
-  description = "Location of the resource group."
-}
-
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-# outputs.tf
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+    environment = "example"
+  }
 }
